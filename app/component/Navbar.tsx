7e61600a70ecfdb97e96620db1480674adc6a5b8
adilmalik01 <malikadil5679@gmail.com>
2024-08-09 14:52:04 -0700

    complete routing

---
+++ app/component/Navbar.tsx
@@ -0,0 +1,31 @@
+
+
+
+
+const Navbar = () => {
+    return (
+        <nav className="bg-slate-700 flex justify-center  items-center  w-full h-[10vh]">
+            <ul className="flex justify-center  items-center   gap-10 text-white text-[22px] font-semibold">
+                <li><a href="/">Home</a></li>
+                <li><a href="./about">About</a></li>
+                <li><a href="./contact">Contact</a></li>
+            </ul>
+        </nav>
+
+    );
+}
+
+export default Navbar;
+
+
+
+
+
+
+
+
+
+
+
+
+
